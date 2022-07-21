3d5d39f01fb28cf964448234ddfde5f8232550b6
Daniel Villacis <daniel1997v@gmail.com>
2022-07-21 02:14:36 -0400

    reservationRepository additions

--- src/main/java/com/xbstrxct/flightservices/integration/ReservationRestController.java
+++ src/main/java/com/xbstrxct/flightservices/integration/ReservationRestController.java
@@ -3,6 +3,7 @@ package com.xbstrxct.flightservices.integration;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +34,7 @@ public class ReservationRestController {
 	}
 
 	@RequestMapping(value = "/reservations", method = RequestMethod.POST)
+	@Transactional	// "All should happen or nothing should happen"
 	public Reservation saveReservation(CreateReservationRequest request) {
 		Flight flight = flightRepository.findById(request.getFlightId()).get(); // Will find the flight by the id.
 

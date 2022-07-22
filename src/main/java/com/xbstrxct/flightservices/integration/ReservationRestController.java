704e1cbda515bcd2fab05d9f24828186cf0ff52c
Daniel Villacis <daniel1997v@gmail.com>
2022-07-22 11:50:14 -0400

    FlightServices backend API and structure completed

--- src/main/java/com/xbstrxct/flightservices/integration/ReservationRestController.java
+++ src/main/java/com/xbstrxct/flightservices/integration/ReservationRestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import com.xbstrxct.flightservices.repos.PassengerRepository;
 import com.xbstrxct.flightservices.repos.ReservationRepository;
 
 @RestController
+@CrossOrigin
 public class ReservationRestController {
 
 	@Autowired
@@ -39,6 +41,12 @@ public class ReservationRestController {
 			@RequestParam("departureDate") @DateTimeFormat(pattern = "MM-dd-yyyy") Date departureDate) {
 		return flightRepository.findFlights(from, to, departureDate); // Will return a list of all flights and their
 																		// details
+	}
+
+	@RequestMapping(value = "/flights/{id}")
+	public Flight findFlight(@PathVariable("id") int id) {
+		return flightRepository.findById(id).get();
+
 	}
 
 	@RequestMapping(value = "/reservations", method = RequestMethod.POST)
